c552e7e413ce9e35ab8f2f52bbd234ba0a5e278d
oukrithy <oukrithy@yahoo.com>
2017-06-16 09:09:41 -0700

    added ships

--- battle_ship.js
+++ battle_ship.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  randomizerShip();
+  // randomizerShip();
   var hits = 0
   for(var i = 0; i <= 9; i++) {
     let row = "<tr id=row" + i +">column</tr>"
@@ -40,7 +40,7 @@ $(document).ready(function(){
       $("#result").text("Winner!!!!!!!!")
     }
 
-    if (torpedoUsed < 25) {
+    if (torpedoUsed < 1) {
       console.log(torpedoUsed);
       $("#torpedos").text("Torpedos Used: " + torpedoUsed)
     } else {
@@ -72,8 +72,11 @@ var torpedoUsed = 0
 var shipLocation = []
 var shipCombo = []
 
+//----------------------------------------------
+//         CREATING A ONE LENTH SHIP
+//----------------------------------------------
 function randomizerShip() {
-  for (var i = 1; i<=5; i++) {
+  for (var i = 1; i<=1; i++) {  // changed to only place one ship
   do {
       var shipRow = Math.floor(Math.random()*7 + 1)
       var shipCol = Math.floor(Math.random()*7 + 1)
@@ -93,8 +96,6 @@ function randomizerShip() {
 
     var ship = '#' + shipRow + "" + shipCol
     shipLocation.push(ship)
-
-
     }
 shipMaker()
 }
@@ -104,4 +105,166 @@ function shipMaker() {
 }
 
 //----------------------------------------------
+//         CREATING A TWO LENTH SHIP
 //----------------------------------------------
+function randomizerTwoLenthShip() {
+  for (var i = 1; i<=1; i++) {  // changed to only place one ship
+  do {
+      var shipRow = Math.floor(Math.random()*7 + 1)
+      var shipCol = Math.floor(Math.random()*6 + 1)
+      var shipColAddOne = shipCol + 1;
+    } while (board[shipRow][shipCol] === 1 ||
+             board[shipRow][shipCol] === 2 );
+
+    board[shipRow][shipCol] = 1
+    board[shipRow][shipColAddOne] = 1
+
+    board[shipRow - 1][shipCol] = 2
+    board[shipRow  + 1][shipCol] = 2
+    board[shipRow][shipCol - 1 ] = 2
+    board[shipRow][shipCol + 1 ] = 2
+    board[shipRow - 1][shipCol - 1] = 2
+    board[shipRow  - 1][shipCol +1] = 2
+    board[shipRow +1][shipCol - 1 ] = 2
+    board[shipRow +1][shipCol + 1 ] = 2
+
+    var ship = '#' + shipRow + "" + shipCol
+    var shipTwo = '#' + shipRow + "" + shipColAddOne
+    shipLocation.push(ship)
+    shipLocation.push(shipTwo)
+
+    }
+shipMaker()
+}
+//----------------------------------------------
+//         CREATING A THREE LENTH SHIP
+//----------------------------------------------
+function randomizerThreeLenthShip() {
+  for (var i = 1; i<=1; i++) {  // changed to only place one ship
+  do {
+      var shipRow = Math.floor(Math.random()*7 + 1)
+      var shipCol = Math.floor(Math.random()*5 + 1)
+      var shipColAddOne = shipCol + 1;
+      var shipColAddTwo= shipCol + 2;
+    } while (board[shipRow][shipCol] === 1 ||
+             board[shipRow][shipCol] === 2 );
+
+    board[shipRow][shipCol] = 1
+    board[shipRow][shipColAddOne] = 1
+    board[shipRow][shipColAddTwo] = 1
+
+    board[shipRow - 1][shipCol] = 2
+    board[shipRow  + 1][shipCol] = 2
+    board[shipRow][shipCol - 1 ] = 2
+    board[shipRow][shipCol + 1 ] = 2
+    board[shipRow - 1][shipCol - 1] = 2
+    board[shipRow  - 1][shipCol +1] = 2
+    board[shipRow +1][shipCol - 1 ] = 2
+    board[shipRow +1][shipCol + 1 ] = 2
+
+    var ship = '#' + shipRow + "" + shipCol
+    var shipTwo = '#' + shipRow + "" + shipColAddOne
+    var shipThree = '#' + shipRow + "" + shipColAddTwo
+
+    shipLocation.push(ship)
+    shipLocation.push(shipTwo)
+    shipLocation.push(shipThree)
+
+    }
+shipMaker()
+}
+//----------------------------------------------
+//         CREATING A FOUR LENTH SHIP
+//----------------------------------------------
+function randomizerFourLenthShip() {
+  for (var i = 1; i<=1; i++) {  // changed to only place one ship
+  do {
+      var shipRow = Math.floor(Math.random()*7 + 1)
+      var shipCol = Math.floor(Math.random()*4 + 1)
+      var shipColAddOne = shipCol + 1;
+      var shipColAddTwo= shipCol + 2;
+      var shipColAddThree= shipCol + 3;
+    } while (board[shipRow][shipCol] === 1 ||
+             board[shipRow][shipCol] === 2 );
+
+    board[shipRow - 1][shipCol] = 2
+    board[shipRow  + 1][shipCol] = 2
+    board[shipRow][shipCol - 1 ] = 2
+    board[shipRow][shipCol + 1 ] = 2
+    board[shipRow - 1][shipCol - 1] = 2
+    board[shipRow  - 1][shipCol +1] = 2
+    board[shipRow +1][shipCol - 1 ] = 2
+    board[shipRow +1][shipCol + 1 ] = 2
+
+    board[shipRow][shipCol] = 1
+    board[shipRow][shipColAddOne] = 1
+    board[shipRow][shipColAddTwo] = 1
+    board[shipRow][shipColAddThree] = 1
+
+    var ship = '#' + shipRow + "" + shipCol
+    var shipTwo = '#' + shipRow + "" + shipColAddOne
+    var shipThree = '#' + shipRow + "" + shipColAddTwo
+    var shipFour = '#' + shipRow + "" + shipColAddThree
+
+    shipLocation.push(ship)
+    shipLocation.push(shipTwo)
+    shipLocation.push(shipThree)
+    shipLocation.push(shipFour)
+
+    }
+shipMaker()
+}
+//----------------------------------------------
+//         CREATING A FIVE LENTH SHIP *** NEEDS TO REVISE -- FAILS/BREAKS
+//----------------------------------------------
+function randomizerFiveLenthShip() {
+  for (var i = 1; i<=1; i++) {  // changed to only place one ship
+  do {
+      var shipRow = Math.floor(Math.random()*7 + 1)
+      var shipCol = Math.floor(Math.random()*3 + 1)
+      var shipColAddOne = shipCol + 1;
+      var shipColAddTwo= shipCol + 2;
+      var shipColAddThree= shipCol + 3;
+      var shipColAddFour= shipCol + 4;
+
+    } while (board[shipRow][shipCol] === 1          ||
+             board[shipRow][shipColAddOne] === 1    ||
+             board[shipRow][shipColAddTwo] === 1    ||
+             board[shipRow][shipColAddThree] === 1  ||
+             board[shipRow][shipColAddFour] === 1   ||
+             board[shipRow][shipCol] === 2          ||
+             board[shipRow][shipColAddOne] === 2    ||
+             board[shipRow][shipColAddTwo] === 2    ||
+             board[shipRow][shipColAddThree] === 2  ||
+             board[shipRow][shipColAddFour] === 2 );
+
+    board[shipRow][shipCol] = 1
+    board[shipRow][shipColAddOne] = 1
+    board[shipRow][shipColAddTwo] = 1
+    board[shipRow][shipColAddThree] = 1
+    board[shipRow][shipColAddFour] = 1
+
+    board[shipRow - 1][shipCol] = 2
+    board[shipRow  + 1][shipCol] = 2
+    board[shipRow][shipCol - 1 ] = 2
+    board[shipRow][shipCol + 1 ] = 2
+    board[shipRow - 1][shipCol - 1] = 2
+    board[shipRow  - 1][shipCol +1] = 2
+    board[shipRow +1][shipCol - 1 ] = 2
+    board[shipRow +1][shipCol + 1 ] = 2
+
+    var ship = '#' + shipRow + "" + shipCol
+    var shipTwo = '#' + shipRow + "" + shipColAddOne
+    var shipThree = '#' + shipRow + "" + shipColAddTwo
+    var shipFour = '#' + shipRow + "" + shipColAddThree
+    var shipFive = '#' + shipRow + "" + shipColAddFour
+
+    shipLocation.push(ship)
+    shipLocation.push(shipTwo)
+    shipLocation.push(shipThree)
+    shipLocation.push(shipFour)
+    shipLocation.push(shipFive)
+
+    }
+shipMaker()
+}
